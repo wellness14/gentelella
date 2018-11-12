b13b439a7469943cfffe4ce158f86fbabdc2f30c
wellness14 <ezh29@mamcho.co.kr>
2018-11-12 09:41:45 +0900

    first

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    // sass = require('gulp-ruby-sass'),
+    Fiber = require("fibers"),
+    sass = require('gulp-dart-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    sourcemaps = require('gulp-sourcemaps');
+
+// var DEST = 'build/';
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'src/js/helpers/*.js',
+        'src/js/*.js',
+      ])
+      .pipe(concat('custom.js'))
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(browserSync.stream());
+});
+
+// TODO: Maybe we can simplify how sass compile the minify and unminify version
+// var compileSASS = function (filename, options) {
+//   return sass('src/scss/*.scss', options)
+//         .pipe(autoprefixer('last 2 versions', '> 5%'))
+//         .pipe(concat(filename))
+//         .pipe(gulp.dest(DEST+'/css'))
+//         .pipe(browserSync.stream());
+// };
+
+
+
+
+
+
+
+// gulp.task('sass', function() {
+//     return compileSASS('custom.css', { sourcemap: false });
+// });
+
+// gulp.task('sass-minify', function() {
+//     return compileSASS('custom.min.css', {style: 'compressed'});
+// });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        startPath: './production/index.html'
+    });
+});
+
+
+
+// gulp.task('default', ['browser-sync', 'watch']);
+
+
+// gulp.task('sass', function () {
+//     return sass.renderSync({
+//         file: 'src/scss/*.scss'
+//     })
+// })
+
+
+
+'use strict';
+
+gulp.task('sass', function () {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'));
+});
+
+// gulp.task('sass:watch', function () {
+//     gulp.watch('src/scss/*.scss', ['sass']);
+// });
+
+gulp.task('watch', function () {
+    // Watch .html files
+    gulp.watch('production/*.html', browserSync.reload);
+    gulp.watch('src/scss/*.scss', browserSync.reload);
+    // Watch .js files
+    gulp.watch('src/js/*.js', ['scripts']);
+    // Watch .scss files
+    //   gulp.watch('src/scss/*.scss', ['sass', 'sass-minify']);
+    gulp.watch('src/scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch','browser-sync']);
---
+++ src/js/myjs.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Login Notisfy
+    // new PNotify({
+    //     title: '서동필님 안녕하세요.',
+    //     text: '신규 쪽지 <a href="#!" class="red"><strong>2</strong></a>건<br/> 주요 일정 <a href="#!" class="red"><strong>3</strong></a>건',
+    //     type: 'info',
+    //     hide: false,
+    //     styling: 'bootstrap3'
+    // });
+    
+})
